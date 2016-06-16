32d0a636930047059eabfbd5747807334dd3c774
Alexandr Rassadin <arassadin@mera.ru>
2016-06-16 16:59:24 +0300

    New ones and common renaming

---
+++ Rassadin/speech_cw2.m
@@ -0,0 +1,10 @@
+[yy, Fs, nbits] = wavread('i.wav');
+y = yy(:, 1);
+y = y(4 * 10000 : 6 * 10000);
+figure(), plot(y);
+k = abs(fft(y));
+x = linspace(0, 0.5, length(k) / 2);
+figure(), plot(x, k(1:length(k)/2));
+figure(), plot(x, 10*log10(k(1:length(k)/2)));
+sd = 10*log10( pwelch(y, rectwin(480), 0) );
+figure(), plot(linspace(0, 0.5, length(sd)), sd);
