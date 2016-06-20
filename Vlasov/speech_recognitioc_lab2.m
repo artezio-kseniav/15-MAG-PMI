4afa3801ca72e2a35ac2baeece79407baef97385
r_vlasov <r.d.vlasov@gmail.com>
2016-06-20 22:12:35 +0300

    speech recognition

---
+++ Vlasov/speech_recognitioc_lab2.m
@@ -0,0 +1,22 @@
+[a, Fs1, nbits1, opts1] = wavread('a.wav');
+[o, Fs2, nbits2, opts2] = wavread('o.wav');
+[u, Fs3, nbits3, opts3] = wavread('u.wav');
+
+std_all=[];
+input = u;
+
+ara = arburg(a, 20);
+
+fa = filter(ara,1,input); 
+std_all(1) = std(fa);
+plot(fa)
+
+aro = arburg(o, 20);
+fo = filter(aro,1,input); 
+std_all(2) = std(fo);
+
+aru = arburg(u, 20);
+fu = filter(aru,1,input); 
+std_all(3) = std(fu);
+
+[a, i] = min(std_all);
